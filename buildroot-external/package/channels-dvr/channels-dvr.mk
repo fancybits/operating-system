2e6968ba482ec6350436295abef1da6409e08d39
Aman Karmani <aman@tmm1.net>
2020-10-05 18:10:18 -0700

    fix paths

--- buildroot-external/package/channels-dvr/channels-dvr.mk
+++ buildroot-external/package/channels-dvr/channels-dvr.mk
@@ -11,8 +11,8 @@ define CHANNELS_DVR_BUILD_CMDS
 endef
 
 define CHANNELS_DVR_INSTALL_TARGET_CMDS
-	dd if=/dev/zero of=/export/data.ext4 bs=4G count=1
-	mkfs.ext4 -L "hassos-data" -E lazy_itable_init=0,lazy_journal_init=0 /export/data.ext4
+	dd if=/dev/zero of=$(BINARIES_DIR)/data.ext4 bs=4G count=1
+	mkfs.ext4 -L "hassos-data" -E lazy_itable_init=0,lazy_journal_init=0 $(BINARIES_DIR)/data.ext4
 endef
 
 $(eval $(generic-package))
