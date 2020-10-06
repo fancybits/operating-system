5c906b9577752211f13502c381a5ad9a8c00d218
Aman Karmani <aman@tmm1.net>
2020-10-05 18:01:22 -0700

    simplify data partition creation

---
+++ buildroot-external/package/channels-dvr/channels-dvr.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# Channels DVR
+#
+################################################################################
+
+CHANNELS_DVR_VERSION = 1.0.0
+
+define CHANNELS_DVR_BUILD_CMDS
+	
+endef
+
+define CHANNELS_DVR_INSTALL_TARGET_CMDS
+	dd if=/dev/zero of=/export/data.ext4 bs=4G count=1
+	mkfs.ext4 -L "hassos-data" -E lazy_itable_init=0,lazy_journal_init=0 /export/data.ext4
+endef
+
+$(eval $(generic-package))
