9f594b63187a5d1c23594bfbf88e6078ce9fac18
Aman Karmani <aman@tmm1.net>
2020-10-08 11:39:34 -0700

    collapse version in the filename

--- buildroot-external/scripts/name.sh
+++ buildroot-external/scripts/name.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 function hassos_image_name() {
-    echo "${BINARIES_DIR}/${HASSOS_ID}_${BOARD_ID}_${VERSION_MAJOR}.${VERSION_BUILD}.${1}"
+    local filenamever = $(hassos_version | sed -e 's,\.,,g')
+    echo "${BINARIES_DIR}/${HASSOS_ID}_${BOARD_ID}_${filenamever}.${1}"
 }
 
 function hassos_rauc_compatible() {
