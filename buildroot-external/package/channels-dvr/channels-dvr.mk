ac3aff9578b2676ca88815382d5017ea502125b0
Aman Karmani <aman@tmm1.net>
2020-10-08 13:34:44 -0700

    bump version

--- buildroot-external/package/channels-dvr/channels-dvr.mk
+++ buildroot-external/package/channels-dvr/channels-dvr.mk
@@ -4,10 +4,9 @@
 #
 ################################################################################
 
-CHANNELS_DVR_VERSION = 1.0.0
+CHANNELS_DVR_VERSION = 2020.10.08.2033
 CHANNELS_DVR_SITE = $(BR2_EXTERNAL_HASSOS_PATH)/package/channels-dvr
 CHANNELS_DVR_SITE_METHOD = local
-CHANNELS_DVR_BUILD_VERSION = 2020.10.08.0153
 
 define CHANNELS_DVR_USERS
 	channels 501 channels 501 * /mnt/data/channels-dvr/data - video Channels
@@ -20,14 +19,14 @@ define CHANNELS_DVR_INSTALL_TARGET_CMDS
 	mkdir -p /mnt/data/
 	sudo mount -o loop $(BINARIES_DIR)/data.ext4 /mnt/data
 
-	sudo mkdir -p /mnt/data/channels-dvr/{data,$(CHANNELS_DVR_BUILD_VERSION)}
-	sudo curl "https://channels-dvr.s3.amazonaws.com/$(CHANNELS_DVR_BUILD_VERSION)/ffmpeg-linux-arm64" -o /mnt/data/channels-dvr/$(CHANNELS_DVR_BUILD_VERSION)/ffmpeg
-	sudo curl "https://channels-dvr.s3.amazonaws.com/$(CHANNELS_DVR_BUILD_VERSION)/ffprobe-linux-arm64" -o /mnt/data/channels-dvr/$(CHANNELS_DVR_BUILD_VERSION)/ffprobe
-	sudo curl "https://channels-dvr.s3.amazonaws.com/$(CHANNELS_DVR_BUILD_VERSION)/comskip-linux-arm64" -o /mnt/data/channels-dvr/$(CHANNELS_DVR_BUILD_VERSION)/comskip
-	sudo curl "https://channels-dvr.s3.amazonaws.com/$(CHANNELS_DVR_BUILD_VERSION)/channels-dvr-linux-arm64" -o /mnt/data/channels-dvr/$(CHANNELS_DVR_BUILD_VERSION)/channels-dvr
-	sudo chmod +x /mnt/data/channels-dvr/$(CHANNELS_DVR_BUILD_VERSION)/*
-	sudo curl -f -s "https://channels-dvr.s3.amazonaws.com/$(CHANNELS_DVR_BUILD_VERSION)/linux-arm64.sha256" -o /mnt/data/channels-dvr/$(CHANNELS_DVR_BUILD_VERSION)/linux-arm64.sha256
-	sudo ln -nsf /mnt/data/channels-dvr/$(CHANNELS_DVR_BUILD_VERSION) /mnt/data/channels-dvr/latest
+	sudo mkdir -p /mnt/data/channels-dvr/{data,$(CHANNELS_DVR_VERSION)}
+	sudo curl "https://channels-dvr.s3.amazonaws.com/$(CHANNELS_DVR_VERSION)/ffmpeg-linux-arm64" -o /mnt/data/channels-dvr/$(CHANNELS_DVR_VERSION)/ffmpeg
+	sudo curl "https://channels-dvr.s3.amazonaws.com/$(CHANNELS_DVR_VERSION)/ffprobe-linux-arm64" -o /mnt/data/channels-dvr/$(CHANNELS_DVR_VERSION)/ffprobe
+	sudo curl "https://channels-dvr.s3.amazonaws.com/$(CHANNELS_DVR_VERSION)/comskip-linux-arm64" -o /mnt/data/channels-dvr/$(CHANNELS_DVR_VERSION)/comskip
+	sudo curl "https://channels-dvr.s3.amazonaws.com/$(CHANNELS_DVR_VERSION)/channels-dvr-linux-arm64" -o /mnt/data/channels-dvr/$(CHANNELS_DVR_VERSION)/channels-dvr
+	sudo chmod +x /mnt/data/channels-dvr/$(CHANNELS_DVR_VERSION)/*
+	sudo curl -f -s "https://channels-dvr.s3.amazonaws.com/$(CHANNELS_DVR_VERSION)/linux-arm64.sha256" -o /mnt/data/channels-dvr/$(CHANNELS_DVR_VERSION)/linux-arm64.sha256
+	sudo ln -nsf /mnt/data/channels-dvr/$(CHANNELS_DVR_VERSION) /mnt/data/channels-dvr/latest
 	sudo chown -R 501:501 /mnt/data/channels-dvr
 
 	if ! sudo umount /mnt/data; then \
