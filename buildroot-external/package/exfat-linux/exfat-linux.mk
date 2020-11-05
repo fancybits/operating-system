68a6415636cbefe9acd55366fab57b405ff7ffe4
Aman Karmani <aman@tmm1.net>
2020-11-05 15:50:43 -0800

    add exfat-linux kernel module

---
+++ buildroot-external/package/exfat-linux/exfat-linux.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# exfat-linux
+#
+################################################################################
+
+EXFAT_LINUX_VERSION = 034f47d607c441e7fb5be3eb71d7a289ef76bc0c
+EXFAT_LINUX_SITE = $(call github,arter97,exfat-linux,$(EXFAT_LINUX_VERSION))
+EXFAT_LINUX_LICENSE = GPL-2.0
+EXFAT_LINUX_LICENSE_FILES = LICENSE
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
