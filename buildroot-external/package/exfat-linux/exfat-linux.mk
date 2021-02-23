3660bd52bd3ad618de6f483fda25bad06e7a701b
Aman Karmani <aman@tmm1.net>
2021-02-23 10:54:35 -0800

    switch to better maintained exfat driver

--- buildroot-external/package/exfat-linux/exfat-linux.mk
+++ buildroot-external/package/exfat-linux/exfat-linux.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-EXFAT_LINUX_VERSION = 034f47d607c441e7fb5be3eb71d7a289ef76bc0c
-EXFAT_LINUX_SITE = $(call github,arter97,exfat-linux,$(EXFAT_LINUX_VERSION))
+EXFAT_LINUX_VERSION = 4a5c1b2064f2ceb03c34c94f9b331191e8683cee
+EXFAT_LINUX_SITE = $(call github,namjaejeon,linux-exfat-oot,$(EXFAT_LINUX_VERSION))
 EXFAT_LINUX_LICENSE = GPL-2.0
 EXFAT_LINUX_LICENSE_FILES = LICENSE
 EXFAT_LINUX_MODULE_MAKE_OPTS = CONFIG_EXFAT_FS=m CONFIG_EXFAT_VIRTUAL_XATTR=y
